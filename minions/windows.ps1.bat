7d83e3317a5d927f7896c24a045402a213a99836
Colin Stubbs <colin-stubbs@users.noreply.github.com>
2018-08-20 22:18:35 +1000

    Update windows.ps1.bat

--- minions/windows.ps1.bat
+++ minions/windows.ps1.bat
@@ -4,7 +4,7 @@ REM We need to force unrestricted execution policy here
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
 
 REM Run powershell from URL
-powershell https://files.routedlogic.net/salt/bootstrap/windows.ps1
+powershell https://github.com/colin-stubbs/salt-bootstrap/blob/master/minions/windows.ps1
 
 REM Change execution policy back to restricted
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Restricted -Force"
